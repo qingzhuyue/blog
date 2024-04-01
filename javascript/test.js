09305c268a0030b60a03a4952238a11d4f7136b8
qingzhuyue <qingzhuyue@foxmail.com>
2024-04-01 21:44:39 +0800

    面试题

---
+++ javascript/test.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: qingzhuyue qingzhuyue@foxmail.com
+ * @Date: 2024-03-26 21:06:31
+ * @LastEditors: qingzhuyue qingzhuyue@foxmail.com
+ * @LastEditTime: 2024-03-26 21:06:33
+ * @FilePath: /blog/javascript/test.js
+ * @Description: 
+ * Copyright (c) 2024 by ${qingzhuyue} email: ${qingzhuyue@foxmail.com}, All Rights Reserved.
+ */
+ Promise((resolve,reject)=>{})
